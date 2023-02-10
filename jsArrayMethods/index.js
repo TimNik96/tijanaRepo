e3837c58de7c06ac237e05229539405ac6d7cb52
ntim <timotic.nikola.27@gmail.com>
2023-02-10 16:45:13 +0100

    add unfinished methods

--- jsArrayMethods/index.js
+++ jsArrayMethods/index.js
@@ -139,4 +139,61 @@ let niz = [1, 5, 11, 3]
 
 // console.log(nizStr.sort())
 // console.log(niz.sort((a, b) => b - a))
-// console.log(nizObj.sort((a, b) => a.name - b.name)) // NE RADI!! Uneti ispravku
+// console.log(nizObj.sort((a, b) => a.name - b.name)) // NE RADI
+// ISPRAVKA
+const items = [
+    { name: "Edward", value: 21 },
+    { name: "Sharpe", value: 37 },
+    { name: "And", value: 45 },
+    { name: "The", value: -12 },
+    { name: "Magnetic", value: 13 },
+    { name: "Zeros", value: 37 },
+];
+
+console.log(items.sort((a, b) => {
+    const nameA = a.name // ignore upper and lowercase
+    const nameB = b.name // ignore upper and lowercase
+    if (nameA > nameB) {
+        return 1;
+    }
+    if (nameA < nameB) {
+        return -1;
+    }
+    // names must be equal
+    return 0;
+}))
+
+// console.log(niz.join(''))
+// let str = 'danas je lep dan'
+// console.log(str.split(' '))
+
+// const str = 'Jayesh'
+
+// function foo(str) {
+//     if(str.length < 2) {
+//         return str
+//     }
+
+//     return foo(str.slice(1)) + str[0]
+// }
+
+// const result = foo(str)
+// console.log(result)
+
+// niz.fill('mojStr',0)
+// console.log(niz);
+
+// console.log(niz.reduce((a, b) => a + b, 0))
+
+// let suma = 0
+// for(let i = 0; i < niz.length; i++) {
+//     suma += niz[i]
+// }
+
+// console.log(suma)
+
+// niz.forEach(el => {
+//     suma += el
+// })
+
+// console.log(suma)
