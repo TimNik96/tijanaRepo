8d6245bf50b47edf53371e2e33d7da15dff5ebeb
ntim <timotic.nikola.27@gmail.com>
2023-04-30 19:33:22 +0200

    function retro

---
+++ js/funkcije/index.js
@@ -0,0 +1,30 @@
+// function imeFunkcije(parametri) {
+//     return vrednost
+// }
+
+// function saberi(a, b = 0) {
+//     let zbir = a + b
+//     return zbir
+// }
+
+// const saberi = (a, b) => {     // Drugi nacin
+//     return a + b
+// }
+
+// let sabirak1 = 10
+// let sabirak2 = 20
+
+// let zbir = saberi(sabirak1, sabirak2)
+// console.log(zbir)
+
+// console.log(saberi(sabirak1, sabirak2))
+
+// function ispisiIme(ime, prezime) {
+//     console.log(ime + ' ' + prezime)
+// }
+
+// let name = 'Tijana'
+// let surname = 'Djukic'
+
+// ispisiIme(name, surname)
+
