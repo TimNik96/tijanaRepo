c8b125f8f7785c0835d6afac1ac06926eddec824
ntim <timotic.nikola.27@gmail.com>
2023-05-03 00:52:11 +0200

    arrays retrospective

---
+++ js/nizovi/index.js
@@ -0,0 +1,70 @@
+// let niz = [1, 2, 3, 4, 'string', [1, 2, 3], {ime: 'Marko'}]
+
+// for(let i = 0; i < niz.length; i++) {
+//     console.log(niz[i]);
+// }
+
+// niz.forEach(element => {
+//     console.log(niz[i])
+// })
+
+// let niz = [10, 21, 33, 45, 400, 8749]
+// let suma = 0
+// niz.forEach(el => {
+//     suma += el
+// })
+// console.log(suma)
+
+// for(let i = 0; i < niz.length; i++) {
+//     if(i % 2 == 0)
+//         suma += niz[i]
+// }
+
+// console.log(suma)
+
+// for(let i = 0; i < niz.length; i++) {
+//     if(niz[i] % 2 != 0)
+//         suma += niz[i]
+// }
+
+// console.log(suma)
+
+
+// let imena = ['Jana', 'Stasa', 'Uros', 'Slaven']
+// let imeNaSlovo = 'S'
+
+// for(let i = 0; i < imena.length; i++) {
+//     if(imena[i].charAt(0) === imeNaSlovo)
+//         console.log(imena[i])
+// }
+
+// for(let i = 0; i < imena.length; i++) {
+//     if(imena[i].length > 5)
+//         console.log(imena[i])
+// }
+
+let niz = [1, 2, 3, 4, 1, 1, 4, 7, 1]
+let trazeniEl = 1
+let brojac = 0
+
+// for(let i = 0; i < niz.length; i++) {
+//     if(niz[i] == trazeniEl)
+//         brojac++
+// }
+
+// console.log(brojac)
+
+
+// for(let i = 0; i < niz.length; i++) {
+//     if(niz[i] === trazeniEl) {
+//         if(brojac != 0) {
+//             niz.splice(i, 1)
+//             i--
+//         } else {
+//             brojac++
+//         }
+//     }
+// }
+
+// console.log(niz)
+
